bd607c679dde6d6d5e00f4f3e8727e6a09f64776
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-06 21:48:38 -0500

    Update helloworld-handle.js

--- helloworld-handle.js
+++ helloworld-handle.js
@@ -5,26 +5,11 @@ var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 3221);
 
 app.get('/', function (req, res) {
-    res.render('home.handlebars') //We can omit the .handlebars extension as we do below
-});
-
-app.get('/other-page', function (req, res) {
-    res.render('other-page');
-});
-
-app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
-});
-
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.render('500');
+    res.type('text/plain');
+    res.send('Welcome to the main page!');
 });
 
 app.listen(app.get('port'), function () {
