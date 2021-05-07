33b677cf7773d9bca90a5eee269462ec73653ea8
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-06 22:49:52 -0500

    Update helloworld-handle.js

--- helloworld-handle.js
+++ helloworld-handle.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express();
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+var handlebars = require('express-handlebars')
+
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
