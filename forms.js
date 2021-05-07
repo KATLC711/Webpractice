7e97f6b03beae671cb395dbc36e4c36baf29446a
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-06 23:01:42 -0500

    Update forms.js

--- forms.js
+++ forms.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());*/
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 3221);
 
 app.get('/', function (req, res) {
     res.render('home');
