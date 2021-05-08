ee1d3e4ee0586a7019ecef77ca089f74b3a3c2d3
KATLC711 <71677779+KATLC711@users.noreply.github.com>
2021-05-07 22:37:40 -0500

    Update

---
+++ get_post_request.js
@@ -0,0 +1,35 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({ defaultLayout: 'get_post_main' });
+
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3221);
+
+
+function get_time() {
+    return (new Date(Date.now())).toLocaleTimeString('en-US');
+}
+
+
+app.get('/', function (req, res) {
+
+    var context = {
+        'time': get_time(),
+        'greet': "Kevin Cheung - Assignment 5"
+    }
+
+    res.render('home', context);
+});
+
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
